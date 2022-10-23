e86731f608a9a3a43c440d291b74d76d11543ba5
Figglophobia <gideon.jaffe@gmail.com>
2022-10-24 00:34:27 +0300

    a little fix

--- src/main/java/Week2/PhoneBook/PhoneBook/PhoneBook.java
+++ src/main/java/Week2/PhoneBook/PhoneBook/PhoneBook.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PhoneBook {
-    private List<Contact> contacts;
+    private final List<Contact> contacts;
 
     private final String name;
 
