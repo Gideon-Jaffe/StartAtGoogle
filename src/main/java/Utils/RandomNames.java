11ad232b40509085cfcfb722444b595e15994ca0
Figglophobia <gideon.jaffe@gmail.com>
2022-10-26 16:03:44 +0300

    Changed Random names to use gson

--- src/main/java/Utils/RandomNames.java
+++ src/main/java/Utils/RandomNames.java
@@ -1,36 +1,37 @@
 package Utils;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONValue;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.*;
+import java.lang.reflect.Type;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomNames {
 
-    private final JSONArray names;
+    private final List<String> jsonNames;
 
     public RandomNames(String path) {
-        JSONArray tempNames;
         try (Reader tempReader = new FileReader(path)) {
-            tempNames = (JSONArray) JSONValue.parse(tempReader);
+            Gson gson = new Gson();
+            Type stringList = new TypeToken<List<String>>(){}.getType();
+            jsonNames = gson.fromJson(tempReader, stringList);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(path + " not found");
         } catch (IOException e) {
             throw new RuntimeException("IO Error in opening " + path);
         }
-
-        names = tempNames;
     }
 
     public String getRandomNameFromJsonFile() {
-        if (names == null) return "";
-        return (String) names.get(ThreadLocalRandom.current().nextInt(names.size()));
+        if (jsonNames == null) return "";
+        return jsonNames.get(ThreadLocalRandom.current().nextInt(jsonNames.size()));
     }
 
     public boolean isNameInJsonFile(String name) {
-        if (names == null) return false;
-        return names.contains(name);
+        if (jsonNames == null) return false;
+        return jsonNames.contains(name);
     }
 }
 
